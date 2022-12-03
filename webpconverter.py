6e804f2bc94bced471b5987d88246ce2b4b968a1
ivey kun <44329173+iveykun@users.noreply.github.com>
2022-12-02 23:09:01 -0500

    Add files via upload

---
+++ webpconverter.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+import glob
+import datetime
+counter=338
+location = "C:/Users/yukev/Documents/other_anime/"
+x = datetime.datetime.now()
+# for every item in this folder that ends with .webp
+for item in glob.iglob(r'C:/Users/yukev/Documents/other_anime/*.webp'):
+    print("Original:")
+    print(item)
+    #open and convert to RGB
+    im = Image.open(item).convert("RGB")
+    # new unique name for the picture
+    name = location + x.strftime("%f")+str(counter) + ".png"
+    counter = counter+1
+    print("New:")
+    print(name)
+    im.save(name,"png")
+    #delete the original webp
+    os.remove(item)
+    print("--------")
