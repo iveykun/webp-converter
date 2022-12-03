61ae50f6894d9e62deb0fdf0f02d8ae94b90b280
ivey kun <44329173+iveykun@users.noreply.github.com>
2022-12-02 23:13:20 -0500

    Update webpconverter.py

--- webpconverter.py
+++ webpconverter.py
@@ -2,19 +2,19 @@ from PIL import Image
 import os
 import glob
 import datetime
-counter=338
-location = "C:/Users/yukev/Documents/"
+counter=924
+savelocation = "C:/Users/yukev/Documents/"
 x = datetime.datetime.now()
 # for every item in this folder that ends with .webp
 for item in glob.iglob(r'C:/Users/yukev/Documents/*.webp'):
     print("Original:")
-    print(item)
+    #print(item)
     #open and convert to RGB
     im = Image.open(item).convert("RGB")
     # new unique name for the picture
-    name = location + x.strftime("%f")+str(counter) + ".png"
+    name = savelocation + x.strftime("%f")+str(counter) + ".png"
     counter = counter+1
-    print("New:")
+    #print("New:")
     print(name)
     im.save(name,"png")
     #delete the original webp
